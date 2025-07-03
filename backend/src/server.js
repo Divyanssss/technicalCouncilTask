178ad5b352154c4208ac78de0601c69d071ddf02
Divyanssss <divyanship19@gmail.com>
2025-07-03 20:37:40 +0530

    serving static file from backend/dist

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,26 @@ import dotenv from "dotenv";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from './routes/userRoutes.js';
 import { connectDB } from "./config/db.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use("/events", eventRoutes);
 app.use('/users', userRoutes);
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 connectDB().then(() => {
   app.listen(PORT, '0.0.0.0', () => {
